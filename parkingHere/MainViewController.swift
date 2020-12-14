ab4020abd94ebcacd91de3ef8a9aa6d3d251fc53
Juyeon <remake867@gmail.com>
2020-12-15 01:10:36 +0900

    feature: run parking time

--- parkingHere/MainViewController.swift
+++ parkingHere/MainViewController.swift
@@ -8,20 +8,14 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    var isParking: Bool = false
     var hasImage: Bool = false
-
-    @IBAction func didTapAlarmButton(_ sender: Any) {
-        datePicker.isHidden = !datePicker.isHidden
-    }
-    @IBOutlet weak var alarmButton: UIButton!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var carImage: UIImageView!
-    @IBOutlet weak var alarmTime: UILabel!
-    @IBOutlet weak var memo: UITextView!
+    
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var memoTextField: UITextView!
     @IBOutlet weak var startParkingButton: UIButton!
     @IBOutlet weak var addCarButton: UIButton!
     @IBOutlet weak var resetCarButton: UIButton!
+    
     @IBAction func didTapResetCarButton(_ sender: Any) {
         let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .actionSheet)
 
@@ -34,7 +28,7 @@ class MainViewController: UIViewController {
             (alert: UIAlertAction!) -> Void in
             self.addCarButton.isHidden = false
             self.resetCarButton.isHidden =  true
-            self.carImage.image = UIImage(systemName: "car.fill")
+            self.carImageView.image = UIImage(systemName: "car.fill")
         })
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {
@@ -46,18 +40,27 @@ class MainViewController: UIViewController {
         
         self.present(optionMenu, animated: true, completion: nil)
     }
+    
     @IBAction func didTabAddCarButton(_ sender: Any) {
         self.openCamera()
     }
     
-    @IBAction func didTapStartParkingButton(_ sender: Any) {
-        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "ParkingVC") else {
-            return
-        }
-        vc.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
-        vc.modalPresentationStyle = .fullScreen
+    fileprivate func saveParkingInformation() {
+        UserDefaults.standard.set(true, forKey: "isParking")
+        UserDefaults.standard.set(Date(), forKey: "parkingTime")
         
-        self.present(vc, animated: true)
+        if memoTextField.text != "간단메모" {
+            UserDefaults.standard.set(memoTextField.text, forKey: "memo")
+        }
+        
+        if hasImage {
+            UserDefaults.standard.set(carImageView.image?.pngData(), forKey: "carImage")
+        }
+    }
+    
+    @IBAction func didTapStartParkingButton(_ sender: Any) {
+        saveParkingInformation()
+        goParkingVC()
     }
     
     func openCamera() {
@@ -68,11 +71,13 @@ class MainViewController: UIViewController {
         present(vc, animated: true)
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startParkingButton.layer.cornerRadius = startParkingButton.bounds.height / 2
+        
+        memoTextField.textColor = .gray
+        memoTextField.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -85,11 +90,35 @@ class MainViewController: UIViewController {
             addCarButton.isHidden = false
             resetCarButton.isHidden = true
         }
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(goParkingVC),
+                                               name: UIApplication.willEnterForegroundNotification,
+                                               object: nil)
+    }
+    
+    @objc func goParkingVC() {
+        if UserDefaults.standard.bool(forKey: "isParking") {
+            NotificationCenter.default.removeObserver(self)
+            
+            guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "ParkingVC") as? ParkingViewController else { return }
+            vc.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+            vc.modalPresentationStyle = .fullScreen
+            
+            if let carPNG = UserDefaults.standard.data(forKey: "carImage") {
+                vc.carImage = UIImage(data: carPNG)
+            } else {
+                vc.carImage = UIImage(systemName: "car.fill")
+            }
+            
+            if memoTextField.text != "간단메모" {
+                vc.memoText = memoTextField.text
+            }
+            self.present(vc, animated: true)
+        }
     }
 }
 
 extension MainViewController: UINavigationControllerDelegate {
-    
 }
 
 extension MainViewController: UIImagePickerControllerDelegate {
@@ -100,7 +129,16 @@ extension MainViewController: UIImagePickerControllerDelegate {
                 print("No image found")
                 return
             }
-        self.carImage.image = image
+        self.carImageView.image = image
         hasImage = true
+    }
+}
+
+extension MainViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "간단 메모" {
+            textView.text = ""
+        }
+        textView.textColor = .label
     }
 }
--- parkingHere/ParkingViewController.swift
+++ parkingHere/ParkingViewController.swift
@@ -8,14 +8,84 @@
 import UIKit
 
 class ParkingViewController: UIViewController {
-    @IBOutlet weak var endParkingButton: UIButton!
+    typealias HHMMSS = (HH: Int, MM: Int, SS: Int)
     
-    @IBAction func didTabENdParkingButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    var memoText: String?
+    var secondParking: Int = 0
+    var carImage: UIImage?
+    
+    @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var endParkingButton: UIButton!
+    @IBOutlet weak var parkingTimeLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var trackCarButton: UIButton!
+    @IBAction func didTapTrackCarButton(_ sender: Any) {
     }
+    
+    fileprivate func initParkingInformation() {
+        UserDefaults.standard.set(false, forKey: "isParking")
+        UserDefaults.standard.set(nil, forKey: "parkingTime")
+        UserDefaults.standard.set(nil, forKey: "memo")
+        UserDefaults.standard.set(nil, forKey: "carImage")
+    }
+    
+    @IBAction func didTabEndParkingButton(_ sender: Any) {
+        let preVC = self.presentingViewController
+        guard let vc = preVC as? MainViewController else {
+            return
+        }
+        
+        vc.carImageView.image = UIImage(systemName: "car.fill")
+        vc.hasImage = false
+        self.dismiss(animated: true, completion: {
+            self.initParkingInformation()
+        })
+    }
+    
+    fileprivate func getSecondParking() -> Int {
+        let date = UserDefaults.standard.object(forKey: "parkingTime") as? Date ?? Date()
+        return Int(Date().timeIntervalSince1970 - date.timeIntervalSince1970)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         endParkingButton.layer.cornerRadius = endParkingButton.bounds.height / 2
+        trackCarButton.layer.cornerRadius = trackCarButton.bounds.height / 2
+        
+        if let carImage = carImage {
+            self.carImageView.image = carImage
+        }
+        
+        if let memoText = memoText {
+            memoLabel.text = memoText
+        } else {
+            memoLabel.isHidden = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Timer.scheduledTimer(timeInterval: 1.0,
+                             target: self,
+                             selector: #selector(updateCounter),
+                             userInfo: nil,
+                             repeats: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    func secondsToHoursMinutesSeconds (seconds : Int) -> HHMMSS {
+      return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    }
+    
+    @objc func updateCounter() {
+        var secondParking = getSecondParking()
+        secondParking += 1
+        let HHMMSS: HHMMSS = secondsToHoursMinutesSeconds(seconds: secondParking)
+        parkingTimeLabel.text =  String(format: "%02d:%02d:%02d", HHMMSS.HH, HHMMSS.MM, HHMMSS.SS)
     }
 }
